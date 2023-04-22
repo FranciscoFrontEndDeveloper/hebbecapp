0c0c0dfe97db735fbc7c1399ccf0fdef6cf64cab
FRANCISCO <pachonetworking@gmail.com>
2023-04-21 20:35:31 -0500

    label password email finished

--- src/app/components/labelform/labelform.component.ts
+++ src/app/components/labelform/labelform.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-labelform',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./labelform.component.sass']
 })
 export class LabelformComponent {
-
+  @Input('titleLabel') titleLabel: string ="titulo del label";
 }
--- src/app/components/loginform/loginform.component.ts
+++ src/app/components/loginform/loginform.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
-  styleUrls: ['./loginform.component.sass']
+  styleUrls: ['./loginform.component.sass'],
 })
 export class LoginformComponent {
-
+  labelEmail: string = 'Email';
+  labelPassword: string = 'Password';
 }
